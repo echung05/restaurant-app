1e1cae818e9a8f210349f6e57ced8b3c1ba227a1
Dakota Bourne <44448765+dakotabourne373@users.noreply.github.com>
2020-05-25 13:54:35 -0400

    added hardcoded objects to map through

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Accordion from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+const DisplayPlaces = (props) => {
+  return (
+    <div className="placesList">
+      <Accordion>
+        {props.places.map(place => {
+          return (
+            <Card>
+              <Card.Header>
+                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                  {place.name}
+                </Accordion.Toggle>
+              </Card.Header>
+              <Accordion.Collapse eventKey="0">
+                <Card.Body>{place.rating}</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+          )
+        })}
+      </Accordion>
+    </div>
+  );
+}
+
 function App() {
+  const [getPlaces, setPlaces] = React.useState(
+    [{
+      name: "The Virginian",
+      rating: "4.3/5",
+      location: "1521 University Ave",
+      price: "$$",
+      type: "American restaurant",
+    }, {
+      name: "Roots",
+      rating: "4.7/5",
+      location: "1329 Main St W",
+      price: "$",
+      type: "Restaurant",
+    }, {
+      name: "Christians",
+      rating: "4/5",
+      location: "100 14th St NW",
+      price: "$",
+      type: "Pizza Restaurant",
+    }]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <DisplayPlaces places={getPlaces} />
+
     </div>
   );
 }
