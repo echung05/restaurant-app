4edf9c05247fa98d4c9991f4ffca3e2f7a6660f1
Dakota Bourne <44448765+dakotabourne373@users.noreply.github.com>
2020-05-25 11:50:08 -0400

    Updates

---
+++ precache-manifest.60623dc1dcbba61005f3c61aa06e9f7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538b94b3cd02e7febb53b2a7bf353117",
+    "url": "/echung05.github.io/restaurant-app/index.html"
+  },
+  {
+    "revision": "242365053d35f31a3b6a",
+    "url": "/echung05.github.io/restaurant-app/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "7267549fa2a2a287da3d",
+    "url": "/echung05.github.io/restaurant-app/static/js/2.ec46a666.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/echung05.github.io/restaurant-app/static/js/2.ec46a666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242365053d35f31a3b6a",
+    "url": "/echung05.github.io/restaurant-app/static/js/main.6d4a002e.chunk.js"
+  },
+  {
+    "revision": "ce544296a5e83782aa81",
+    "url": "/echung05.github.io/restaurant-app/static/js/runtime-main.5c989299.js"
+  }
+]);
