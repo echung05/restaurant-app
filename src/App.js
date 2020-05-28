4f40d55399fd328a066768e4f912b57d7197d332
Dakota Bourne <44448765+dakotabourne373@users.noreply.github.com>
2020-05-28 13:31:04 -0400

    finished up main functionality

--- src/App.js
+++ src/App.js
@@ -17,27 +17,33 @@ const DisplayPlaces = (props) => {
       <Accordion>
         {props.places.map((place, index) => {
           return (
-            <Card>
-              <Card.Header>
-                <Accordion.Toggle as={Button} variant="link" eventKey={index}>
-                  <div>{place.name}</div>
-                  <div style={{ fontSize: "12px", textAlign: "left", fontWeight: "bold" }}>
-                    {
-                      place.price_level === 4 ? <div>$$$$</div> :
-                        place.price_level === 3 ? <div>$$$</div> :
-                          place.price_level === 2 ? <div>$$</div> :
-                            <div>$</div>
-                    }
-                  </div>
-                </Accordion.Toggle>
-              </Card.Header>
-              <Accordion.Collapse eventKey={index}>
-                <Card.Body>
-                  <div style={{ fontSize: "12px", textAlign: "left" }}>{place.rating} &#9733; ({place.user_ratings_total}) on Yelp</div>
-                  <div style={{ fontSize: "12px", textAlign: "left", fontWeight: "bold" }}>{place.vicinity}</div>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
+            <div>
+              {
+                (place.rating >= props.rating) && [
+                  <Card>
+                    <Card.Header>
+                      <Accordion.Toggle as={Button} variant="link" eventKey={index}>
+                        <div>{place.name}</div>
+                        <div style={{ fontSize: "12px", textAlign: "left", fontWeight: "bold" }}>
+                          {
+                            place.price_level === 4 ? <div>$$$$</div> :
+                              place.price_level === 3 ? <div>$$$</div> :
+                                place.price_level === 2 ? <div>$$</div> :
+                                  <div>$</div>
+                          }
+                        </div>
+                      </Accordion.Toggle>
+                    </Card.Header>
+                    <Accordion.Collapse eventKey={index}>
+                      <Card.Body>
+                        <div style={{ fontSize: "12px", textAlign: "left" }}>{place.rating} &#9733; ({place.user_ratings_total}) on Yelp</div>
+                        <div style={{ fontSize: "12px", textAlign: "left", fontWeight: "bold" }}>{place.vicinity}</div>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                ]
+              }
+            </div>
           )
         })}
       </Accordion>
@@ -49,6 +55,7 @@ function App() {
 
   const [getPlaces, setPlace] = React.useState([]);
   const [getCoords, setCoords] = React.useState();
+  const [getRating, setRating] = React.useState(0);
 
 
   return (
@@ -56,16 +63,16 @@ function App() {
       <h1 className="header" style={{ fontWeight: "bold", marginTop: "2vh" }}>HOOS Eating</h1>
 
 
-      <div style={{ marginTop: "2vw" }}><InputForm places={getPlaces} setPlaces={setPlace} setCoords={setCoords} /></div>
+      <div style={{ marginTop: "2vw" }}><InputForm places={getPlaces} setPlaces={setPlace} setCoords={setCoords} setRating={setRating} rating={getRating} /></div>
       {
-        (getPlaces != 0) && [
-          <Container fluid>
+        (getPlaces.length !== 0) && [
+          <Container fluid style={{ marginBottom: "8vh" }}>
             <Row>
-              <Col md='auto' style={{ marginTop: "4vh" }}>
-                <DisplayPlaces places={getPlaces} />
+              <Col md='auto' style={{ marginTop: "4.5vh" }}>
+                <DisplayPlaces places={getPlaces} rating={getRating} />
               </Col>
               <Col>
-                <Leaflet coords={getCoords} places={getPlaces} />
+                <Leaflet coords={getCoords} places={getPlaces} rating={getRating} />
               </Col>
             </Row>
           </Container>
--- src/Leaflet.js
+++ src/Leaflet.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Map, Marker, Popup, TileLayer, Circle } from 'react-leaflet'
 import './App.css'
 
-const position = [51.505, -0.09]
 const style = {
     margin: "0 auto",
     width: '100%',
@@ -10,39 +9,38 @@ const style = {
     marginTop: "5vh",
     float: "none"
 }
-export default class Leaflet extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <Map center={this.props.coords} zoom={13} style={style}>
-                <TileLayer
-                    url="https://b.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
-                <Circle color='green' fillOpacity={0.3} radius={200} opacity={0.6} center={this.props.coords} >
-                    <Popup>You are in this area!</Popup>
-                </Circle>
-                {this.props.places.map((place, index) => {
-                    let lat = place.geometry.location.lat;
-                    let lng = place.geometry.location.lng;
-                    let newCoords = [lat, lng];
-                    return (
-                        < div >
-                            <Marker position={newCoords} >
-                                <Popup>
-                                    {place.name}
-                                    <br />{place.rating}&#9733; ({place.user_ratings_total}) on Yelp  <br />{place.vicinity}
-                                </Popup>
-                            </Marker>
-
-                        </div>
-                    );
-                })
-                }
-            </Map >
-        );
-    }
+function Leaflet(props) {
+    let coords = props.coords;
+    return (
+        <Map center={coords} zoom={13} style={style}>
+            <TileLayer
+                url="https://b.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            />
+            <Circle color='green' fillOpacity={0.3} radius={200} opacity={0.6} center={coords} >
+                <Popup>You are in this area!</Popup>
+            </Circle>
+            {props.places.map((place, index) => {
+                let lat = place.geometry.location.lat;
+                let lng = place.geometry.location.lng;
+                let newCoords = [lat, lng];
+                return (
+                    < div >
+                        {
+                            (place.rating >= props.rating) && [
+                                <Marker position={newCoords} >
+                                    <Popup>
+                                        {place.name}
+                                        <br />{place.rating}&#9733; ({place.user_ratings_total}) on Yelp  <br />{place.vicinity}
+                                    </Popup>
+                                </Marker>
+                            ]
+                        }
+                    </div>
+                );
+            })
+            }
+        </Map >
+    );
 }
+export default Leaflet;
